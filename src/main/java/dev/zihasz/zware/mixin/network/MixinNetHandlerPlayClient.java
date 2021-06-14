5b33a3bf4d5dc7bc3dd91cb63e267fc8025a2186
zihasz <ihaszzeno@gmail.com>
2021-06-14 21:47:09 +0200

    Fix MixinNetHandlerPlayClient

--- src/main/java/dev/zihasz/zware/mixin/network/MixinNetHandlerPlayClient.java
+++ src/main/java/dev/zihasz/zware/mixin/network/MixinNetHandlerPlayClient.java
@@ -1,13 +1,10 @@
 package dev.zihasz.zware.mixin.network;
 
-import com.mojang.authlib.GameProfile;
 import dev.zihasz.zware.features.module.misc.FakeLunar;
 import dev.zihasz.zware.manager.ModuleManager;
 import io.netty.buffer.Unpooled;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.network.NetworkManager;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.client.CPacketCustomPayload;
 import net.minecraft.network.play.server.SPacketJoinGame;
@@ -17,18 +14,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(NetHandlerPlayClient.class)
-public class MixinNetHandlerPlayClient extends NetHandlerPlayClient {
-
-	public MixinNetHandlerPlayClient(Minecraft mc, GuiScreen screen, NetworkManager manager, GameProfile profile) {
-		super(mc, screen, manager, profile);
-	}
+public class MixinNetHandlerPlayClient {
 
 	@Inject(method = "handleJoinGame", at = @At("HEAD"), cancellable = true)
 	public void handleJoinGame(SPacketJoinGame packetJoinGame, CallbackInfo info) {
 		if (ModuleManager.getModule(FakeLunar.class).isEnabled()) {
-			this.client.player.connection.sendPacket(new CPacketCustomPayload(
+			Minecraft.getMinecraft().player.connection.sendPacket(new CPacketCustomPayload(
 					"Lunar-Client",
-					new PacketBuffer(Unpooled.buffer()).writeByteArray(this.client.session.getPlayerID().getBytes())
+					new PacketBuffer(Unpooled.buffer()).writeByteArray(Minecraft.getMinecraft().session.getPlayerID().getBytes())
 			));
 			System.out.println("Sent CPacketCustomPayload");
 		}
