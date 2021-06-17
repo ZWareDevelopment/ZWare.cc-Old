b9d4c5ca4bf3e43c76cfc9966696887847ca74b3
zihasz <ihaszzeno@gmail.com>
2021-06-17 17:30:54 +0200

    Fix FakeLunar

--- src/main/java/dev/zihasz/zware/features/module/misc/FakeLunar.java
+++ src/main/java/dev/zihasz/zware/features/module/misc/FakeLunar.java
@@ -1,8 +1,15 @@
 package dev.zihasz.zware.features.module.misc;
 
+import dev.zihasz.zware.event.events.PacketEvent;
 import dev.zihasz.zware.features.module.Category;
 import dev.zihasz.zware.features.module.Module;
 import dev.zihasz.zware.manager.ModuleManager;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.network.Packet;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.client.CPacketCustomPayload;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class FakeLunar extends Module {
 
@@ -14,4 +21,18 @@ public class FakeLunar extends Module {
 	public void onEnable() {
 		ModuleManager.getModule(FakeVanilla.class).enable();
 	}
+
+	@SubscribeEvent
+	public void onPacketSend(PacketEvent.Send event) {
+		Packet<?> raw = event.getPacket();
+		if(raw instanceof CPacketCustomPayload) {
+			CPacketCustomPayload packet = (CPacketCustomPayload) raw;
+			if(packet.getChannelName().equalsIgnoreCase("MC|Brand")) {
+				ByteBuf message = Unpooled.buffer();
+				message.writeBytes("Lunar-Client".getBytes());
+				mc.player.connection.sendPacket(new CPacketCustomPayload("REGISTER", new PacketBuffer(message)));
+			}
+		}
+	}
+
 }
