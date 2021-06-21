b8fe5324a878aa623407993b11aa5aefbd5e5ecf
zihasz <ihaszzeno@gmail.com>
2021-06-21 10:58:59 +0200

    Add FallDistance option to nofall

--- src/main/java/dev/zihasz/zware/features/module/player/NoFall.java
+++ src/main/java/dev/zihasz/zware/features/module/player/NoFall.java
@@ -3,6 +3,7 @@ package dev.zihasz.zware.features.module.player;
 import dev.zihasz.zware.event.events.PacketEvent;
 import dev.zihasz.zware.features.module.Category;
 import dev.zihasz.zware.features.module.Module;
+import dev.zihasz.zware.features.setting.Setting;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -12,9 +13,11 @@ public class NoFall extends Module {
 		super("NoFall", "Take no fall damage like a god.", Category.PLAYER);
 	}
 
+	private final Setting<Float> fallDistance = new Setting<>("Fall Distance", "Minimum fall distance to disable fall damage.", 3f, 0f, 100f);
+
 	@SubscribeEvent
 	public void onPackerSend(PacketEvent.Read event) {
-		if (event.getPacket() instanceof CPacketPlayer && mc.player.fallDistance >= 3.0) {
+		if (event.getPacket() instanceof CPacketPlayer && mc.player.fallDistance >= fallDistance.getValue()) {
 			CPacketPlayer packet = (CPacketPlayer) event.getPacket();
 			packet.onGround = true;
 		}
